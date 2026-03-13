dc140b172bf7b66904a84563d4021c6b8b0baa68
João Gnoatto <joaogabrielgnoatto2@gmail.com>
2026-03-12 22:07:29 -0300

    Adicionado Alemon.java

---
+++ Alemon.java
@@ -0,0 +1,10 @@
+public class Alemon {
+    public static void HalloWelt(){
+        System.out.println("Hallo Welt!");
+    }
+
+    public static void main(String[] args){
+        HalloWelt();
+    }
+
+}
