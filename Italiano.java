605d7a63c919d2901d409b7ef3d8a592f7fdde6c
João Gnoatto <joaogabrielgnoatto2@gmail.com>
2026-03-12 23:39:11 -0300

    Adicionado git-basico italiano

---
+++ Italiano.java
@@ -0,0 +1,8 @@
+public class Italiano {
+    public static void Italian(){
+        System.out.println("Ciao Mondo!");
+    }
+    public static void main(String[] args){
+        Italian();
+    }
+}
